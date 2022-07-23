7df751a10277e1e3e25a2911f26f6aad15027dec
yunze <yunzez@uw.edu>
2022-07-22 22:58:13 -0700

    update fetch

--- steam-stats-web/src/components/Home.jsx
+++ steam-stats-web/src/components/Home.jsx
@@ -1,20 +1,59 @@
-import { createEffect, createSignal } from "solid-js"
+import { createEffect, createSignal, Suspense, lazy, createResource, For } from "solid-js"
 import { userID, setUserID, login, setLogin, user, setUser } from '../App';
 
-const [id, setID] = createSignal('')
+
+const [games, setGames] = createSignal('')
+const [gamesNum, setGamesNum] = createSignal('')
+// const Games = lazy(async () => {
+//     const resGameList = await fetch(`https://note-the-project-n.herokuapp.com/steam/getinfo?type=gamelist`)
+//     const dataGameList = await resGameList.json()
+//     console.log(dataGameList)
+
+//     const resOwnedGameList = await fetch(`https://note-the-project-n.herokuapp.com/steam/getinfo?type=ownedgame&uid=${userID()}`)
+//     const dataOwnedGameList = await resOwnedGameList.json()
+//     console.log(dataOwnedGameList.info.response)
+//     setGames(dataOwnedGameList.info.response.games)
+//     setGamesNum(dataOwnedGameList.info.response.game_count)
+//     console.log(games(), gamesNum())
+
+//     return(
+//         <div>
+//            <h5>Hi There, {games()}</h5>
+//         </div>
+//     )
+// });
+
+const fetchAllGame = async (id) => {
+    let gamesDataraw = await fetch(`https://note-the-project-n.herokuapp.com/steam/getinfo?type=ownedgame&uid=${id}`);
+    let gameDataCleaned = await gamesDataraw.json()
+    console.log(gameDataCleaned.info.response)
+    setGames(gameDataCleaned.info.response.games)
+    setGamesNum(gameDataCleaned.info.response.game_count)
+    return gameDataCleaned.info.response
+}
+
+
 const Home = () => {
-    createEffect(async()=>{
-        const res = await fetch(`https://note-the-project-n.herokuapp.com/steam/getinfo?type=gamelist`)
-        const data = await res.json()
-        console.log(data)
-    })
-    console.log(user())
-    let userData = user()
+    console.log(user(), userID())
+    let userData = user()['0']
+    let totalGameNum = 0
+
+    const [gamesFetch] = createResource(userID(), fetchAllGame);
+    console.log(games())
     return (
-        <div>
-            <h2>Home</h2>
-            <h5>{user()[0].personaname}</h5>
-            <img src={user()[0].avatarfull}></img>
+        <div class='container shadow'>
+            <h4><b>Hi there,</b> {userData.personaname}</h4>
+            <h5>{userData.loccountrycode}</h5>
+            <img src={userData.avatarfull}></img>
+            <span>{gamesFetch.loading && <h1>"Loading..."</h1>}</span>
+            <div>
+                <pre>{JSON.stringify(games())}</pre>
+            </div>
+            <Suspense fallback={<h1>Loading...</h1>}>
+                <For each={games()}>
+                    {(game) => { game }}
+                </For>
+            </Suspense>
         </div>
     )
 }
